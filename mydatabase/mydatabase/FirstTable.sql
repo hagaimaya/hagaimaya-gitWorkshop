e3cc1d736f824be306166bc34df4cab2438f58fe
Hagai Maya Ben Ezra <hagaimaya@microsoft.com>
2022-05-26 12:24:49 +0300

    aa

--- mydatabase/mydatabase/FirstTable.sql
+++ mydatabase/mydatabase/FirstTable.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [firstName] NCHAR(10) NULL, 
     [lastName] NCHAR(10) NULL, 
-    [address] NCHAR(10) NULL
+    [address] NCHAR(10) NULL, 
+    [anothecolumn] NCHAR(10) NULL
 )
